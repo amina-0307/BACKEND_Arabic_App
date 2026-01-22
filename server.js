286259c2dc607fef5e1fccc4705b351a6df1177f
Amina Saleem <amina_0307@hotmail.com>
2026-01-22 15:18:04 +0000

    Fixing ERROR 500

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/.*/", cors(corsOptions)); // IMPORTANT: regex, not "*" //
+app.options(/.*/, cors(corsOptions)); // IMPORTANT: regex, not "*" //
 
 /* OPENAI CLIENT */
 let client;
